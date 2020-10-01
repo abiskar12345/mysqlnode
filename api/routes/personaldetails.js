2cf3c272ae3f10327e30239e1d48218744423d86
Rajan Karki <krajantn@gmail.com>
2020-10-01 21:21:55 +0545

    check personal detail setup check and formatted response too

--- api/routes/personaldetails.js
+++ api/routes/personaldetails.js
@@ -22,31 +22,58 @@ router.post("/:email", Auth, (req, res, next) => {
         });
       } else {
         pool.query(
-          "insert into personal_details( _email,_name,_birthdate,_age,_martialstatus,_height,_religion,_gender,_country, _languages)  values(?,?,?,?,?,?,?,?,?,?) ",
+          "insert into personal_details( _email,_firstName,_lastName,_birthdate,_martialstatus,_height,_weight,_religion,_gender,_country, _city,_qualification,_profession,_bio,_profilePhoto)  values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) ",
           [
             req.params.email,
-            req.body.name,
+            req.body.firstName,
+            req.body.lastName,
             req.body.birthdate,
-            req.body.age,
-            req.body.maritalstatus,
+            req.body.maritalStatus,
             req.body.height,
+            req.body.weight,
             req.body.religion,
             req.body.gender,
             req.body.country,
-            req.body.languages,
+            req.body.city,
+            req.body.qualification,
+            req.body.profession,
+            req.body.bio,
+            req.body.profilePhoto,
           ],
           function (error, result) {
             if (error) {
               res.status(500).json({
                 error: error,
               });
-            } else {
-              res.status(201).json({
-                status: "Success",
-                message: "Add personal details successfully",
-                result,
-              });
             }
+            console.log(req.params.email);
+            pool.query(
+              "select _email,_password from tbl_user where _email = ?",
+              [req.params.email],
+              function (err, user, fields) {
+                console.log(req.params.email);
+                console.log(user[0]);
+                console.log(user[1]);
+                console.log(err);
+                console.log(fields);
+                if (err) {
+                  console.log("Here");
+                  res.status(404).json({
+                    message: "Not found",
+                  });
+                } else {
+                  const data = {
+                    email: user[0]._email,
+                    password: user[0]._password,
+                  };
+                  res.status(201).json({
+                    status: "Success",
+                    message: "Add personal details successfully",
+                    data,
+                  });
+                }
+              }
+            );
           }
         );
       }
@@ -108,9 +135,9 @@ router.patch("/:email", (req, res, next) => {
 });
 
 // check if user already setup their details
-router.get("/checkSetup/:email",(req, res, next) => {
-  console.log("checking")
-  console.log(req.params.email)
+router.get("/checkSetup/:email", (req, res, next) => {
+  console.log("checking");
+  console.log(req.params.email);
   pool.query(
     "SELECT * FROM personal_details WHERE _email=?",
     [req.params.email],
