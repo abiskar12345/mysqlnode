acbd2f81406d64a7570514013c804839cb066890
abiskar12345 <37185280+abiskar12345@users.noreply.github.com>
2020-10-07 15:34:32 +0545

    socket connection pdated

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ const app = require('./app');
 const socketio = require("socket.io");
 
 const socketEvents = require('./api/socket/socket'); 
-
+ 
 
 const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 const socket =socketio(server);
 
-socket.on('connection', client => {
-  client.on('event', data => { /* … */ });
-  client.on('disconnect', () => { /* … */ });
-});
+
+
+  new socketEvents(socket).socketConfig();
+  
+ 
+
 
 server.listen(port);
 
