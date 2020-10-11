5fd942448948801b4903a1e9287551ed2db972b1
Rajan Karki <krajantn@gmail.com>
2020-10-11 21:56:15 +0545

    get logged in user perosnal details

--- api/routes/personaldetails.js
+++ api/routes/personaldetails.js
@@ -5,8 +5,7 @@ const pool = require("../config/database");
 const Auth = require("../auth/authorization");
 const isAuthorized = require("../auth/profileathoruize");
 
-router.post("/:email", Auth,isAuthorized, (req, res, next) => {
-
+router.post("/:email", Auth, isAuthorized, (req, res, next) => {
   // console.log(req.body);
   pool.query(
     "SELECT * FROM personal_details WHERE _email = ?",
@@ -53,17 +52,35 @@ router.post("/:email", Auth,isAuthorized, (req, res, next) => {
             // req.body.languages,
           ],
           function (error, result) {
-            console.log(`this is detail error : ${error}`)
+            console.log(`this is detail error : ${error}`);
             if (error) {
               res.status(500).json({
                 error: error,
               });
             } else {
-              res.status(201).json({
-                status: "Success",
-                message: "Add personal details successfully",
-                result,
-              });
+              pool.query(
+                "SELECT * FROM tbl_user WHERE _email = ?",
+                [req.params.email],
+                function (error, user) {
+                  console.log(`last check ${req.body.email}`);
+                  if (error) {
+                    res.status(404).json({
+                      status: "failed",
+                      error,
+                    });
+                  }
+                  const data = {
+                    username: user[0]._username,
+                    email: user[0]._email,
+                    password: user[0]._password,
+                  };
+                  res.status(201).json({
+                    status: "Success",
+                    message: "Add personal details successfully",
+                    data,
+                  });
+                }
+              );
             }
           }
         );
@@ -77,29 +94,61 @@ router.post("/:email", Auth,isAuthorized, (req, res, next) => {
   // {
 });
 
-router.get("/:email",Auth, isAuthorized,  (req, res, next) => {
-  pool.query(
-    "select *  from personal_details where _email = ?",
-    [req.params.email],
-    function (error, user, fields) {
-      console.log(user);
+// router.get("/:email",Auth, isAuthorized,  (req, res, next) => {
+// //   pool.query(
+// //     "select *  from personal_details where _email = ?",
+// //     [req.params.email],
+// //     function (error, user, fields) {
+// //       console.log(user);
 
+// //       if (error) {
+// //         return res.status(401).json({
+// //           status: "error",
+// //           error: error,
+// //         });
+// //       }
+// //       return res.status(200).json({
+// //         status: "Success",
+// //         message: " personal details updated successfully",
+// //          data: user,
+// //       });
+// //     }
+// //   );
+// // }
+
+// )
+
+// get logged in user data
+router.get("/:email", Auth, isAuthorized, (req, res, next) => {
+  pool.query(
+    "SELECT a._username,a._email ,b._firstName,b._lastName,b._gender,b._birthdate,b._height,b._country,b._religion,b._martialstatus FROM tbl_user as a JOIN personal_details as b ON a._email = b._email WHERE a._email = ?",
+    [req.params.email],
+    function (error, user) {
       if (error) {
-        return res.status(401).json({
-          status: "error",
-          error: error,
+      } else {
+        const data = {
+          username:user[0]._username,
+          email:user[0]._email,
+          firstName:user[0]._firstName,
+          lastName:user[0]._lastName,
+          gender:user[0]._gender,
+          birthdate:user[0]._birthdate,
+          height:user[0]._height,
+          country:user[0]._country,
+          religion:user[0]._religion,
+          maritalStatus:user[0]._martialstatus
+        }
+        res.status(200).json({
+          status: "success",
+          message: "Successfully get data",
+          data,
         });
       }
-      return res.status(200).json({
-        status: "Success",
-        message: " personal details updated successfully",
-         data: user,
-      });
     }
   );
 });
 
-router.patch("/:email",Auth, isAuthorized, (req, res, next) => {
+router.patch("/:email", Auth, isAuthorized, (req, res, next) => {
   pool.query(
     "update personal_details set _name=?, _gender=?,_birthdate=?,_age=?,_height=?,_country=?,_religion=?,_martialstatus=?, _languages=?  where _email = ?",
     [
@@ -133,9 +182,9 @@ router.patch("/:email",Auth, isAuthorized, (req, res, next) => {
 });
 
 // check if user already setup their details
-router.get("/checkSetup/:email",(req, res, next) => {
-  console.log("checking")
-  console.log(req.params.email)
+router.get("/checkSetup/:email", (req, res, next) => {
+  console.log("checking");
+  console.log(req.params.email);
   pool.query(
     "SELECT * FROM personal_details WHERE _email=?",
     [req.params.email],
